b66bb072bc6b19caf17543fe6b0824456dd0299b
Devin Blankenship <failure2fly@gmail.com>
2023-07-10 02:14:26 -0700

    added a few images to be used later. Started on js file for a slider to be added later. Right now pushing image as last one no longer shows as it linked to outside source.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,16 @@
+
+$(document).ready(function(){
+  $('.slider-list').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
+
 $(document).ready(function(){
 
   var hero = document.getElementById("landing");
